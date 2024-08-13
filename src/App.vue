0f911f3a7036a2ce06f84e3fb55fb60d0f8c47e0
Guillaume Ferron <guillaume.ferron2@gmail.com>
2024-08-13 11:49:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <main id="app" class="d-flex align-items-center justify-content-center h-full">
+    <div class="wrapper">
+      Start
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@import './scss/index.scss';
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: white;
+  min-height: 100vh;
+}
+</style>
